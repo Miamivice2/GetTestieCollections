70b4b42aed1107a8d76473e092fb5115d680746d
Miamivice2 <andreb.davis2@gmail.com>
2023-03-25 18:24:42 -0400

    made changes understanding the tests

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -1,7 +1,12 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
-import java.util.Stack;
+
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class TestStack {
 
@@ -21,4 +26,44 @@ public class TestStack {
     }
 
     // Make a bigger test exercising more Stack methods.....
-}
+    @Test
+    public void hashMapTest(){
+        HashMap<String,Integer> Map = new HashMap<>();
+        Map.put("Roll",2);
+        Integer Expected = 2;
+        Assert.assertEquals(Expected,Map.get("Roll"));
+
+    }
+    @Test
+   public void ArrayListTest(){
+        ArrayList<Integer> arr = new ArrayList<>();
+            arr.add(5);
+            Integer Expected = 5;
+            Assert.assertEquals(Expected,arr.get(0));
+    }
+    @Test
+    public void HashSetTest(){
+        HashSet<Integer> set = new HashSet<>();
+        set.add(2);
+        Integer Expected = 2;
+        Assert.assertTrue(set.contains(2));
+
+    }
+    @Test
+    public void LinkedListTest(){
+       LinkedList<Integer> list = new LinkedList<>();
+       list.add(5);
+       Integer Expected = 5;
+       Assert.assertTrue(list.add(5));
+    //use assertTrue with lists
+    }
+    @Test
+    public void ArrayDequeTest(){
+      ArrayDeque<Integer>  arr = new ArrayDeque<>();
+      arr.add(8);
+      Integer Expected = 8;
+      Assert.assertTrue(arr.add(8));
+    }
+
+    }
+
